1b0c4f92be5543f9ee47658c3979bd94fe904ccb
odaepo <odaepo@gmail.com>
2015-05-26 11:54:44 +0200

    Initial Configuration of VichUploaderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Tool\Bundle\EntityFileBundle\ToolEntityFileBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
